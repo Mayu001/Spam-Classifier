ed45675aa4ec4a034727232ce47cbeb7376edcaf
Mayank_Ubuntu <mayank.mohit1122@gmail.com>
2018-08-04 13:30:54 +0530

    Training and Testing Done

---
+++ spam-classifier.py
@@ -0,0 +1,34 @@
+import pandas as pd
+from sklearn.feature_extraction.text import CountVectorizer
+from sklearn.model_selection import train_test_split
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.metrics import accuracy_score, precision_score, recall_score, f1_score
+
+df = pd.read_table("SMSSpamCollection.csv", names=['label', 'sms_message'])
+df['label'] = df.label.map({'ham': 0, 'spam': 1})
+X_train, X_test, y_train, y_test = train_test_split(df['sms_message'], df['label'], random_state=1)
+count_vector = CountVectorizer()  # Instantiate the CountVectorizer method
+training_data = count_vector.fit_transform(X_train)  # Fit the training data and then return the matrix
+testing_data = count_vector.transform(X_test)  # Transform testing data and return the matrix.
+# print(testing_data)
+naive_bayes = MultinomialNB()
+naive_bayes.fit(training_data, y_train)
+
+predictions = naive_bayes.predict(testing_data)
+
+print("\nTraining and Testing is Done. Have a look at the accuracy of this model:\n")
+print('Accuracy score: ', format(accuracy_score(y_test, predictions)))
+print('Precision score: ', format(precision_score(y_test, predictions)))
+print('Recall score: ', format(recall_score(y_test, predictions)))
+print('F1 score: ', format(f1_score(y_test, predictions)))
+print("\n *********** Result of your Input File **********\n")
+with open("input.txt") as f:
+    content = f.readlines()
+content = [x.strip() for x in content]
+cleaned_data = count_vector.transform(content)
+predictions = naive_bayes.predict(cleaned_data)
+for i in range(len(predictions)):
+    if predictions[i] == 1:
+        print("Data ", i + 1, ": oops, Its a Spam")
+    else:
+        print("Data ", i + 1, ": Yooo, Its a Ham")
